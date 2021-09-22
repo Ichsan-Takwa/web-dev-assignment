a7a2168b326c8051563516f65a979f270966cdae
Ichsan Takwa <ichsanmg07@gmail.com>
2021-09-22 11:07:13 +0800

    Menghapus folder lama

--- Tugas4/js/script.js
+++
@@ -1,8 +0,0 @@
-const btnSwitch = document.getElementById("btn-switch");
-const switchMode = document.getElementsByClassName("content-wrapper");
-
-btnSwitch.addEventListener("click",() => {
-    for (let i = 0; i< switchMode.length; i++) {
-        switchMode[i].classList.toggle("light");
-    }
-});
